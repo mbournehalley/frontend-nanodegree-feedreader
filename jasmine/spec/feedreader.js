a0529cf9a243f8830ddae90894e236cb8f61b5e3
Bourne <mbournehalley@gmail.com>
2016-10-09 15:05:43 -0700

    Test for RSS Feeds name and URLS.
    Unit test for RSS Feeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,11 +9,14 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
+    'use strict';
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
+        var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+        var url_regex = new RegExp(expression);
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -31,16 +34,55 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('URLs are defined', function () {
+            for(var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+            }
+        });
 
+        it('URLs are not null', function () {
+            for(var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).not.toBeNull();
+            }
+        });
+
+        it('URLs does contain empty strings', function () {
+            for(var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url.length).not.toEqual(0);
+            }
+        });
+
+        it('URLs have correct format', function () {
+            for(var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).toMatch(url_regex);
+            }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('names are defined', function() {
+            for(var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+            }
+        });
+
+        it('names are not null', function () {
+            for(var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].name).not.toBeNull();
+            }
+         });
+
+        it('names does contain empty strings', function () {
+            for(var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].name.length).not.toEqual(0);
+            }
+        });
     });
 
-
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -53,20 +95,24 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+    });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
